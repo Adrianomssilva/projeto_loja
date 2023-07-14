149a3a2818aa2bb07d91bd485801bad77d395e45
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-14 10:24:25 -0300

    Produto cadastrado

--- produto.rb
+++ produto.rb
@@ -3,12 +3,18 @@ class Produto
    attr_accessor :valor
    attr_reader :tipo
    attr_reader :marca
+   attr_accessor :cor
    
-   def initialize (marca, tipo, valor)
+   def initialize (marca, tipo, valor, cor)
       @marca = marca
       @tipo = tipo
       @valor = valor
+      @cor = cor
       
    end
 
+   def to_s
+      "Marca: #{@marca}, Tipo: #{@tipo},Cor: #{@cor},Valor: R$#{@valor}"
+   end
+   
 end
---
+++ teste.rb
@@ -0,0 +1,5 @@
+require_relative "produto"
+
+palete_eudora = Produto.new( "Glam", "Palete", 200.0, "Marrom")
+
+puts palete_eudora
