8c8268cbed1bf211ec690f61b4bd67550b7a7999
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-15 17:21:51 -0300

    classe caixa feita... revisar funcionalidades. (Como eu posso somar varios itens?)

--- caixa.rb
+++ caixa.rb
@@ -1,7 +1,16 @@
 class Caixa
+   attr_accessor :valor
+   attr_reader :resultado
 
    def initialize(valor, resultado, )
      @valor = valor
+     @resultado = resultado
    end
+
+   def soma(valor_1,valor_2)
+
+      resultado = valor_1 + valor_2
+
+
    
 end
