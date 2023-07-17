cb18b687c6d6f525b030e47e4d9eed2c232ebc2d
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-17 05:04:38 -0300

    consegui puxar os valores e somar... tenho que transferir isso para a classe caixa

--- teste.rb
+++ teste.rb
@@ -5,9 +5,22 @@ estoque = Estoque.new
 palete_eudora = Produto.new( "Glam", "Palete", 200.0, "Marrom")
 hidratante_lavictoria = Produto.new("La Victoria", "Hidratante", 50.0, "N/a")
 
- p palete_eudora.produto
+#  p palete_eudora.produto
 
-estoque.adiciona(palete_eudora.tipo, palete_eudora.produto)
-estoque.adiciona(hidratante_lavictoria.tipo, hidratante_lavictoria.produto)
+# ESSA PARTE DO CÓDIGO ESTÁ FUNCIONANDO!
+# estoque.adiciona(palete_eudora.tipo, palete_eudora.produto)
+# estoque.adiciona(hidratante_lavictoria.tipo, hidratante_lavictoria.produto)
 
-estoque.produtos
+
+
+# estoque.produtos
+
+puts palete_eudora.valor
+puts hidratante_lavictoria.valor
+
+def resultado ( valor1, valor2)
+   resultado = valor1 + valor2 
+   puts resultado
+end
+
+resultado(palete_eudora.valor,hidratante_lavictoria.valor)
