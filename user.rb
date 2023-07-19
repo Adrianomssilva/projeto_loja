f76fe8c678b314b184426915a65f75a6a3501b30
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-19 10:32:50 -0300

    criação de uma parte visual mais interativa com o usuário

---
+++ user.rb
@@ -0,0 +1,4 @@
+#  ciar uma parte em que o usuário possa add os dados das compras
+# pensar como o caixa de um supermercado que vai add itens e eles vão sendo contabilizados
+# criar um loop talvez seja uma boa. Começar a estudar ruby and rails para implementar melhorias nessa parte visual
+# Estudar banco de dados tbm
