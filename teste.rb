e46ff5871bc48ec7056526237e0c35479789ade5
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-19 08:59:58 -0300

    mudança de armazenamento

--- teste.rb
+++ teste.rb
@@ -2,25 +2,27 @@ require_relative "produto"
 require_relative "estoque"
 
 estoque = Estoque.new
-palete_eudora = Produto.new( "Glam", "Palete", 200.0, "Marrom")
-hidratante_lavictoria = Produto.new("La Victoria", "Hidratante", 50.0, "N/a")
+palete_eudora = Produto.new( "Glam", :palete, 200.0, "Marrom")
+hidratante_lavictoria = Produto.new("La Victoria", :hidratante, 50.0, "N/a")
+hidratante_victoriasecret = Produto.new("Victoria Secret", :hidratante, 100.0, "Rosa")
 
 #  p palete_eudora.produto
 
 # ESSA PARTE DO CÓDIGO ESTÁ FUNCIONANDO!
-# estoque.adiciona(palete_eudora.tipo, palete_eudora.produto)
-# estoque.adiciona(hidratante_lavictoria.tipo, hidratante_lavictoria.produto)
+estoque.adiciona(palete_eudora.tipo, palete_eudora.produto)
+estoque.adiciona(hidratante_lavictoria.tipo, hidratante_lavictoria.produto)
+estoque.adiciona(hidratante_victoriasecret.tipo, hidratante_victoriasecret.produto)
 
 
 
-# estoque.produtos
+estoque.produtos
 
-puts palete_eudora.valor
-puts hidratante_lavictoria.valor
+# puts palete_eudora.valor
+# puts hidratante_lavictoria.valor
 
-def resultado ( valor) |resultado| 
-   resultado += valor
-   puts resultado
-end
+# def resultado ( valor) |resultado| 
+#    resultado += valor
+#    puts resultado
+# end
 
-resultado(palete_eudora.valor,hidratante_lavictoria.valor)
+# resultado(palete_eudora.valor,hidratante_lavictoria.valor)
