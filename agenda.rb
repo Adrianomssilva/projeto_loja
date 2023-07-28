3299005ff46672f8ddc86b04151dc79c04844293
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-28 17:27:48 -0300

    criando uma classe cliente

---
+++ agenda.rb
@@ -0,0 +1,12 @@
+class Agenda
+   def initialize()
+     @agenda = {}
+   end
+
+   def adicionar(user)
+      agenda << user 
+      
+   end
+      
+   
+end
--- user.rb
+++ user.rb
@@ -1,20 +1,12 @@
-class Agenda
+class Cliente
    attr_accessor :nome
    attr_accessor :data_nascimento
    attr_accessor :tel
    
 
-   def initialize()
+   def initialize(nome, data_nascimento, tel)
       @nome = nome
       @data_nascimento = data_nascimento
       @tel = tel
-      @agenda = []
-   end
 
-   def adicionar(user)
-      agenda << user 
-      
    end
-      
-   
-end
